190e2717fb5aa88a62e59817b377fb3bc80d10ff
Raffael Henrique <52468910+raffaelhfarias@users.noreply.github.com>
2022-10-13 14:32:49 -0300

    Update mod28.py

--- mod28.py
+++ mod28.py
@@ -48,7 +48,7 @@ def to_excel(df):
 def main():
     # Configuração inicial da página da aplicação
     st.set_page_config(page_title = 'Telemarketing analisys', \
-        page_icon = "./term-deposit/telmarketing_icon.png",
+        page_icon = "./telmarketing_icon.png",
         layout="wide",
         initial_sidebar_state='expanded'
     )
@@ -58,7 +58,7 @@ def main():
     st.markdown("---")
     
     # Apresenta a imagem na barra lateral da aplicação
-    image = Image.open("./term-deposit/Bank-Branding.jpg")
+    image = Image.open("./Bank-Branding.jpg")
     st.sidebar.image(image)
 
     # Botão para carregar arquivo na aplicação
